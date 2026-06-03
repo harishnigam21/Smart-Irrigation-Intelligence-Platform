a31eabb3a1ec4a176ab8daaf773d882b6a133efd
Harish Nigam <harishnigam21@gmail.com>
2026-06-04 01:58:22 +0530

    Update Auth.ts
    Code cleanup

--- backend/src/controllers/Auth.ts
+++ backend/src/controllers/Auth.ts
@@ -1,11 +1,10 @@
-import Users from "../models/User";
+import Users, { IUser } from "../models/User";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import envVariables from "../envConfig";
 import { getServerError } from "../utils/serverError";
 
-// login controller that takes will be taking email and password from body, check user availability if user exist then check for password using bcrypt because in DB password is encrypted and here in body it is plain text, after successful login it will generate two tokens access and refresh using jwt with user id payload, both will be provided to user access token direct with response and refresh with cookie, refresh have longer live time and access token have shorter time, refresh token helps to generate access token again, refresh is defer with access in terms of access time and also refresh token is stored in DB, so whenever access token expired refresh request will be send it wil check in DB for same refresh token and for valid user it i=will regenerate refresh token
 export const LogIn = async (req: Request, res: Response) => {
   const { email } = req.body;
   try {
@@ -58,20 +57,9 @@ export const LogIn = async (req: Request, res: Response) => {
   }
 };
 
-//Register controller this will take all necessary field from body, check for user existence if not available then encrypt the password using bcrypt and store whole information in DB
 export const Register = async (req: Request, res: Response) => {
   try {
-    const {
-      firstname,
-      middlename,
-      lastname,
-      gender,
-      dob,
-      email,
-      password,
-      fields,
-      sensors,
-    } = req.body;
+    const { firstname, lastname, email, password } = req.body;
     const userExist = await Users.findOne({ email });
 
     if (userExist) {
@@ -82,16 +70,12 @@ export const Register = async (req: Request, res: Response) => {
       return res.status(403).json({ message: "Email ID already exist" });
     }
     const encryptedPassword = await bcrypt.hash(password, 5);
-    const newUser = {
+    const newUser: Omit<IUser, keyof Document> = {
       firstname,
-      middlename: middlename || "",
       lastname,
-      gender,
-      dob,
+      refreshToken: "",
       email,
       password: encryptedPassword,
-      fields: fields || [],
-      sensors: sensors || [],
     };
     const createUser = await Users.create(newUser);
     if (!createUser) {
@@ -106,7 +90,7 @@ export const Register = async (req: Request, res: Response) => {
     getServerError(res, error, "Register Controller");
   }
 };
-//This controller will handle the regeneartion of access token until refresh token is valid, if all thinks good, it will take refresh token from cookie, match refresh token in DB to find out user, if got valid user then verifying it using jwt, if verified regenerating access token for that user and sending it through response
+
 export const handleRefresh = async (req: Request, res: Response) => {
   try {
     const cookies = req.cookies;
@@ -114,7 +98,14 @@ export const handleRefresh = async (req: Request, res: Response) => {
       return res.status(401).json({ message: "Cookie missing" });
     const findUser = await Users.findOne({ refreshToken: cookies.jwt })
       .select("+refreshToken +_id")
-      .lean();
+      .lean<
+        Pick<
+          IUser,
+          "firstname" | "lastname" | "pic" | "email" | "refreshToken"
+        > & {
+          _id: string;
+        }
+      >();
     if (!findUser) {
       res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
       return res.status(403).json({ message: "Invalid payload" });
