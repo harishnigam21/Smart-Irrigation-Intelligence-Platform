e84ed6a4c8d4e5e5d635b672f12ccd994e681973
Harish Nigam <harishnigam21@gmail.com>
2026-06-04 09:29:45 +0530

    Update Farm.ts

--- backend/src/routes/Farm.ts
+++ backend/src/routes/Farm.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import jwtVerifier from "../middlewares/jwtVerifier";
-import { addFarm, deleteFarm, getFarms } from "../controllers/Farm";
+import { addFarm, deleteFarm, getFarm, getFarms } from "../controllers/Farm";
 import farmValidation from "../validations/farmValidation";
 import Validate from "../validations/mongooseIDValidation";
 const router = express.Router();
 router
   .route("/farm")
-  .get(jwtVerifier, getFarms)
   .post(jwtVerifier, farmValidation, addFarm)
   .delete(Validate, jwtVerifier, deleteFarm);
+router.route("/farms").get(jwtVerifier, getFarms);
+router.route("/farm/:id").get(jwtVerifier, getFarm);
 export default router;
